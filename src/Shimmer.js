c46437beb38a585cbb203a4cb4308a7fb59571df
virajnikam01 <virajnikam01@gmail.com>
2023-11-05 15:46:50 +0530

    updated shimmer

--- src/Shimmer.js
+++ src/Shimmer.js
@@ -1,15 +1,10 @@
 const Shimmer = () => {
   return (
-    <div className="flex flex-wrap mx-auto my-8 gap-5 w-10/12">
-      <div className="w-36 h-52 bg-gray-300 rounded"></div>
-      <div className="w-36 h-52 bg-gray-300 rounded"></div>
-      <div className="w-36 h-52 bg-gray-300 rounded"></div>
-      <div className="w-36 h-52 bg-gray-300 rounded"></div>
-      <div className="w-36 h-52 bg-gray-300 rounded"></div>
-      <div className="w-36 h-52 bg-gray-300 rounded"></div>
-      <div className="w-36 h-52 bg-gray-300 rounded"></div>
-      <div className="w-36 h-52 bg-gray-300 rounded"></div>
-      <div className="w-36 h-52 bg-gray-300 rounded"></div>
+    <div className="flex flex-wrap mx-auto my-8 gap-5 w-10/12 py-20 sm:pt-40">
+      <div className="w-36 lg:w-40 h-36 bg-gray-300 rounded"></div>
+      <div className="w-36 lg:w-40 h-36 bg-gray-300 rounded"></div>
+      <div className="w-36 lg:w-40 h-36 bg-gray-300 rounded"></div>
+      <div className="w-36 lg:w-40 h-36 bg-gray-300 rounded"></div>
 
 
     </div>
